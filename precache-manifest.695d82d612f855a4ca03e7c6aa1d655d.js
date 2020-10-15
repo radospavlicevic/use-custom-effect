3c457aee4a73dc3a5928bc7160bb887a2009b226
Rados Pavlicevic <rpavlicevic15@gmail.com>
2020-10-15 16:59:13 +0200

    Updates

---
+++ precache-manifest.695d82d612f855a4ca03e7c6aa1d655d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8407eb49a08228dd9747999aa822b88",
+    "url": "/use-custom-effect/index.html"
+  },
+  {
+    "revision": "3e1742089e92cb1b0887",
+    "url": "/use-custom-effect/static/css/main.c2e0a5f5.chunk.css"
+  },
+  {
+    "revision": "2f28c8f1f606820dd3c9",
+    "url": "/use-custom-effect/static/js/2.b892b0be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/use-custom-effect/static/js/2.b892b0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e1742089e92cb1b0887",
+    "url": "/use-custom-effect/static/js/main.0831007a.chunk.js"
+  },
+  {
+    "revision": "e77f779921abb1b16584",
+    "url": "/use-custom-effect/static/js/runtime-main.bfd30458.js"
+  }
+]);
